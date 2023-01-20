1a346b059c0c24c946880c4a924ce67e6ce2d5c3
Chris Stoughton <chris.stoughton@gmail.com>
2023-01-20 16:26:06 -0600

    convert between channels and frequencies

--- mkids/Mkids.py
+++ mkids/Mkids.py
@@ -2,7 +2,37 @@ import sys
 import MkidsSoc
 import numpy as np
 import xrfdc
+           
+class Mixer():
+    # rf
+    rf = 0
+    
+    def __init__(self, ip):        
+        # Get Mixer Object.
+        self.rf = ip
+    
+    def set_freq(self,f,tile,dac):
+        # Make a copy of mixer settings.
+        dac_mixer = self.rf.dac_tiles[tile].blocks[dac].MixerSettings        
+        new_mixcfg = dac_mixer.copy()
 
+        # Update the copy
+        new_mixcfg.update({
+            'EventSource': xrfdc.EVNT_SRC_IMMEDIATE,
+            'Freq' : f,
+            'MixerType': xrfdc.MIXER_TYPE_FINE,
+            'PhaseOffset' : 0})
+
+        # Update settings.                
+        self.rf.dac_tiles[tile].blocks[dac].MixerSettings = new_mixcfg
+        self.rf.dac_tiles[tile].blocks[dac].UpdateEvent(xrfdc.EVENT_MIXER)
+       
+    def set_nyquist(self,nz,tile,dac):
+        dac_tile = self.rf.dac_tiles[tile]
+        dac_block = dac_tile.blocks[dac]
+        dac_block.NyquistZone = nz          
+
+ 
 class Mkids():
     def __init__(self, soc, decimation=2, streamLength=10000):
         self.soc = soc
@@ -17,7 +47,6 @@ class Mkids():
         self.chsel = self.soc._soc.chsel
         self.board = self.soc.board
 
-        # Setup useful constants
         self.nInCh = self.pfb_in.N
         self.fcIn = self.pfb_in.get_fc()
         self.fbIn = self.pfb_in.get_fb()
@@ -86,34 +115,134 @@ class Mkids():
             self.mixer.set_freq(self.fMixerQuantized,
                                 self.multiTile, 
                                 self.multiBlock)
-           
-class Mixer:    
-    # rf
-    rf = 0
+            
+            
+    def inFreq2ch(self, frequency):
+        """
+        Return the input PFB bin that contains the frequency
+
+        Parameters
+        ----------
+        frequency : double
+            in MHz
+
+        Returns
+        -------
+            ch : int
+                the PFB channel
+        """  
+        N = self.pfb_in.N
+        fc = self.fsIn/N
+        k =np.round(frequency/fc)
+        ch = (np.mod(k+N/2, N)).astype(int)
+        return ch
+
+
+    def inFreq2chOffset(self, frequency):
+        """
+        Return the input PFB bin that contains the frequency, along with the offset from the center
+
+
+        Parameters
+        ----------
+        frequency : double
+            in MHz
+            
+        Returns
+        -------
+            tuple (ch,offset) 
+            
+            ch : int
+                the PFB channel
+            offset : offset
+                offset from the center frequency, in MHz
+        """  
+
+        N = self.pfb_in.N
+        fc = self.fsIn/N
+        k = np.round(frequency/fc)
+        ch = (np.mod(k+N/2, N)).astype(int)
+        fCenter = k*fc
+        offset = frequency-fCenter
+        return ch,offset
     
-    def __init__(self, ip):        
-        # Get Mixer Object.
-        self.rf = ip
+    def inCh2FreqCenter(self, ch):
+        """
+        Converts from input channel number to the frequency at the center of that channel
+
+        Parameters
+        ----------
+        ch : int
+            The PFB channel
+
+        Returns
+        -------
+        fCenter : double
+            frequency at the center of the channel (MHz)
+        """
+   
+        fCenter = np.mod(ch*self.fsIn/self.pfb_in.N + self.fsIn/2, self.fsIn)
+        return fCenter
     
-    def set_freq(self,f,tile,dac):
-        # Make a copy of mixer settings.
-        dac_mixer = self.rf.dac_tiles[tile].blocks[dac].MixerSettings        
-        new_mixcfg = dac_mixer.copy()
+    def outFreq2ch(self, frequency):
+        """
+        Return the output PFB bin that contains the frequency
 
-        # Update the copy
-        new_mixcfg.update({
-            'EventSource': xrfdc.EVNT_SRC_IMMEDIATE,
-            'Freq' : f,
-            'MixerType': xrfdc.MIXER_TYPE_FINE,
-            'PhaseOffset' : 0})
+        Parameters
+        ----------
+        frequency : double
+            in MHz
 
-        # Update settings.                
-        self.rf.dac_tiles[tile].blocks[dac].MixerSettings = new_mixcfg
-        self.rf.dac_tiles[tile].blocks[dac].UpdateEvent(xrfdc.EVENT_MIXER)
-       
-    def set_nyquist(self,nz,tile,dac):
-        dac_tile = self.rf.dac_tiles[tile]
-        dac_block = dac_tile.blocks[dac]
-        dac_block.NyquistZone = nz          
+        Returns
+        -------
+            ch : int
+                the PFB channel
+        """  
+        f = frequency - self.fMixerQuantized
+        ch = self.pfb_out.freq2ch(f)
+        return ch
+    
+    def outFreq2chOffset(self, frequency):
+        """
+        Return the output PFB bin that contains the frequency, along with the offset from the center
 
 
+        Parameters
+        ----------
+        frequency : double
+            in MHz
+            
+        Returns
+        -------
+            tuple (ch,offset) 
+            
+            ch : int
+                the PFB channel
+            offset : offset
+                offset from the center frequency, in MHz
+        """  
+
+        f = frequency - self.fMixerQuantized
+        ch = self.pfb_out.freq2ch(f)
+        fOutCenters = self.outCh2freq(ch)
+        offsets = f - fOutCenters + self.fMixerQuantized
+        return ch,offsets
+ 
+    
+    def outCh2FreqCenter(self, ch):
+        """
+        Converts from output channel number to the frequency at the center of that channel
+
+        Parameters
+        ----------
+        ch : int
+            The PFB channel
+
+        Returns
+        -------
+        fCenter : double
+            frequency at the center of the channel (MHz)
+        """
+        f = self.pfb_out.ch2freq(ch)
+        frequency = f + self.fMixerQuantized
+        return frequency
